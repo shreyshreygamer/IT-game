509f67f9afac82038d945a7c99704daa4a69ea4c
shreyshreygamer <160610346+shreyshreygamer@users.noreply.github.com>
2024-02-20 13:16:18 +0000

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,15 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
+    game.gameOver(false)
+    game.setGameOverEffect(false, effects.blizzard)
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (pinky.vy == 0) {
+        pinky.vy = -150
+    }
+})
+let pinky: Sprite = null
 scene.setBackgroundColor(9)
-let mySprite = sprites.create(img`
+pinky = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . 3 3 3 . . . . . . . . 
     . . . . . 3 . 3 3 3 . . . . . . 
@@ -17,5 +27,7 @@ let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-controller.moveSprite(mySprite)
+controller.moveSprite(pinky, 100, 0)
 tiles.setCurrentTilemap(tilemap`level1`)
+pinky.ay = 350
+scene.cameraFollowSprite(pinky)
