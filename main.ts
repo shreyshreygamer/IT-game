7b927d3fa20ba365d9889a0125cf682f5038f530
shreyshreygamer <160610346+shreyshreygamer@users.noreply.github.com>
2024-03-01 15:02:34 +0000

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,44 +1,37 @@
-scene.onOverlapTile(SpriteKind.Player, assets.tile`
-        myTile0
-    `, function on_overlap_tile(sprite: Sprite, location: tiles.Location) {
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
     game.gameOver(false)
     game.setGameOverEffect(false, effects.blizzard)
 })
-scene.onOverlapTile(SpriteKind.Player, assets.tile`
-        myTile1
-    `, function on_overlap_tile2(sprite2: Sprite, location2: tiles.Location) {
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
     game.gameOver(true)
     game.setGameOverEffect(true, effects.confetti)
 })
-controller.B.onEvent(ControllerButtonEvent.Pressed, function on_b_pressed() {
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     if (pinky.vy == 0) {
         pinky.vy = -200
     }
-    
 })
-let pinky : Sprite = null
+let pinky: Sprite = null
 scene.setBackgroundColor(9)
 pinky = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-            . . . . . 3 3 3 . . . . . . . . 
-            . . . . . 3 . 3 3 3 . . . . . . 
-            . . . . . 3 3 . . 3 . . . . . . 
-            . . . . . . . 3 3 3 . . . . . . 
-            . . . . . . . 3 . . . . . . . . 
-            . . . . . 3 3 3 3 3 3 3 3 3 3 . 
-            . . . . . . . 3 . . . . . . . . 
-            . . . . . . 3 3 3 3 . . . . . . 
-            . . . . . 3 . . . 3 3 . . . . . 
-            . . . . . 3 . . . . 3 . . . . . 
-            . . . . . . . . . . 3 . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . . 
+    . . . . . 3 3 3 . . . . . . . . 
+    . . . . . 3 . 3 3 3 . . . . . . 
+    . . . . . 3 3 . . 3 . . . . . . 
+    . . . . . . . 3 3 3 . . . . . . 
+    . . . . . . . 3 . . . . . . . . 
+    . . . . . 3 3 3 3 3 3 3 3 3 3 . 
+    . . . . . . . 3 . . . . . . . . 
+    . . . . . . 3 3 3 3 . . . . . . 
+    . . . . . 3 . . . 3 3 . . . . . 
+    . . . . . 3 . . . . 3 . . . . . 
+    . . . . . . . . . . 3 . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
 controller.moveSprite(pinky, 100, 0)
-tiles.setCurrentTilemap(tilemap`
-    level1
-`)
+tiles.setCurrentTilemap(tilemap`level1`)
 pinky.ay = 350
 scene.cameraFollowSprite(pinky)
