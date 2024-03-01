b4de8fa430b1362f08ca3f6a1ac9f2b939b33ec2
shreyshreygamer <160610346+shreyshreygamer@users.noreply.github.com>
2024-03-01 14:59:59 +0000

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -2,13 +2,17 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, l
     game.gameOver(false)
     game.setGameOverEffect(false, effects.blizzard)
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    game.gameOver(true)
+    game.setGameOverEffect(true, effects.confetti)
+})
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     if (pinky.vy == 0) {
-        pinky.vy = -150
+        pinky.vy = -200
     }
 })
 let pinky: Sprite = null
-scene.setBackgroundColor(9)
+scene.setBackgroundColor(11)
 pinky = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . 3 3 3 . . . . . . . . 
